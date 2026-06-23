65e632be8245ab333e910f19b950bfcd031b06c4
Sahil Randhawa <sahilrandhawa027@gmail.com>
2026-06-23 22:47:41 +0530

    start

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-// import Homepage from './src/screens/Homepage'
+import Homepage from './src/screens/Homepage'
 import Profile from './src/screens/Profile'
 import Loginpage from './src/auth/Loginpage'
 import Signup from './src/auth/Signup'
 import Setting from './src/screens/Setting'
 const App = () => {
   return (
-    // <Homepage></Homepage>
+    <Homepage></Homepage>
     // <Profile></Profile>
     // <Loginpage></Loginpage>
     //  <Signup></Signup>
-    <Setting></Setting>
+    // <Setting></Setting>
   )
 }
 
---
+++ src/screens/Homepage.js
@@ -0,0 +1,20 @@
+import { View, Text, FlatList,Image,TouchableOpacity,TextInput,StyleSheet} from 'react-native'
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <View style={style.container}>
+     
+    </View>
+  )
+}
+
+export default Homepage
+
+const style=StyleSheet.create({
+
+    container:{
+        flex:1,
+        backgroundColor:"#",
+    }
+})
